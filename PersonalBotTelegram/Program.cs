9bf4421c62f48d3aac3abb2ca9b65903f2c06dea
Carlos <paesleme.carloseduardo@outlook.com>
2018-03-28 20:05:37 -0300

    Criação do Bot com exemplo

--- PersonalBotTelegram/Program.cs
+++ PersonalBotTelegram/Program.cs
@@ -3,13 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
 
 namespace PersonalBotTelegram
 {
     class Program
     {
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("581796064:AAEN11t28iX4yJvnNs1aBY86Cejjed9i2dM");
         static void Main(string[] args)
         {
+            Bot.OnMessage += OnMessage;
+            Bot.OnMessageEdited += OnMessage;
+
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+
+        }
+
+        private static void OnMessage(object sender, MessageEventArgs e)
+        {
+
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.TextMessage)
+            {
+                if (e.Message.Text .ToLower() == "oi" || e.Message.Text.ToLower() == "/start")
+                {
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, "Olá, tudo bem com vc? Vamos malhar ?" + e.Message.Chat.Username);
+
+                }
+                else
+                {
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, "Não entendi, tudo bem com vc? Vamos malhar ?" + e.Message.Chat.Username);
+                }
+            }
         }
     }
 }
